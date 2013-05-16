3b1c101c03b5ee3ea420b335606633173205eae7
guoqing <delguoqing@hotmail.com>
2013-05-16 17:16:12 +0800

    bug fix: batching ordering
    preprocess: order in batch entering order(activate order)
    runtime: order in offset order(time order)

--- src/tja/tja_note_section.py
+++ src/tja/tja_note_section.py
@@ -44,11 +44,11 @@ class CNoteSection(object):
 		return len(self.note_batches) == 0 and len(self._active_batch) == 0
 	
 	def _insert_active_batch(self, src_batch):
-		ins_pos = 0
+		ins_pos = -1
 		for ins_pos, dst_batch in enumerate(self._active_batch):
-			if src_batch > dst_batch:
+			if src_batch.offset > dst_batch.offset:
 				break
-		self._active_batch.insert(ins_pos, src_batch)
+		self._active_batch.insert(ins_pos + 1, src_batch)
 		
 	def update(self, state, onps):
 		# check if new batch will be activated
