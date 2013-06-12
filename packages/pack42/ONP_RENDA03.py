8e33e1a47838f506c4604d0341323542b90aa95f
guoqing <delguoqing@hotmail.com>
2013-06-12 11:25:25 +0800

    remove psp ver data.
    Don't bother to matain 2 version(They use different resource standards)

--- packages/pack42/ONP_RENDA03.py
+++
@@ -1,6 +0,0 @@
-def func0(this, _global):
-	this.stop()
-	
-DATA = (
-	func0,
-)
--- packages/pack95/RENDA_EFFECT_MIKU.py
+++
@@ -1,9 +0,0 @@
-def func0(this, _global):
-	this._root.fscommand("event", "end")
-	this.stop()
-	
-def func1(this, _global):
-	this.stop()
-	
-DATA = (func0, func1)
-
