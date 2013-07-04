069f5820af058d3dd090f8d5c174b4e80a24fbff
GQ <delguoqing@hotmail.com>
2013-07-04 19:23:45 +0800

    finished

--- font/gen_glyph.py
+++ font/gen_glyph.py
@@ -2,13 +2,48 @@ import freetype
 import Image
 import array
 
+FORCE_RGBA = True
+
 def gen0(font_file, pointsize, ch, out_path):
 	face = freetype.Face(font_file)
 	face.set_char_size(pointsize * 64)
 	face.load_char(ch)
 	bitmap = face.glyph.bitmap
 	bitmap_array = array.array('B')
-	bitmap_array.fromlist(bitmap.buffer)
-	bitmap_string = bitmap_array.tostring()
-	image = Image.fromstring('L', (bitmap.width, bitmap.rows), bitmap_string)
-	image.save(out_path)
+	
+	if not FORCE_RGBA and len(bitmap.buffer) > 0:
+		if bitmap.pitch == bitmap.width:
+			bitmap_array.fromlist(bitmap.buffer)
+			bitmap_string = bitmap_array.tostring()			
+			image = Image.fromstring('L', (bitmap.width, bitmap.rows), bitmap_string)
+		elif bitmap.pitch == bitmap.width * 3:
+			bitmap_array.fromlist(bitmap.buffer)
+			bitmap_string = bitmap_array.tostring()						
+			image = Image.fromstring('RGB', (bitmap.width, bitmap.rows), bitmap_string)
+		elif bitmap.pitch == bitmap.width * 4:
+			bitmap_array.fromlist(bitmap.buffer)
+			bitmap_string = bitmap_array.tostring()						
+			image = Image.fromstring('RGBA', (bitmap.width, bitmap.rows), bitmap_string)
+		else:
+			unpacked_buffer = []
+			for j in xrange(bitmap.rows):
+				for i in xrange(bitmap.width):
+					byte = bitmap.buffer[j * bitmap.pitch + i / 8]
+					unpacked_buffer.append((byte & (1 << (7 - i % 8))) != 0 and 255 or 0)
+			bitmap_array.fromlist(unpacked_buffer)
+			bitmap_string = bitmap_array.tostring()
+			#print len(unpacked_buffer), bitmap.width, bitmap.rows
+			image = Image.fromstring('L', (bitmap.width, bitmap.rows), bitmap_string)
+		image.save(out_path)
+		
+	if FORCE_RGBA and len(bitmap.buffer) > 0:
+		assert bitmap.pitch == bitmap.width, "Only support normal pixel mode"
+		expanded_buffer = []
+		for byte in bitmap.buffer:
+			expanded_buffer.extend([255, 255, 255, byte])
+		bitmap_array.fromlist(expanded_buffer)
+		bitmap_string = bitmap_array.tostring()
+		image = Image.fromstring('RGBA', (bitmap.width, bitmap.rows), bitmap_string)
+		image.save(out_path)
+		
+	return bitmap.width, bitmap.rows
--- font/gen_song_name.py
+++ font/gen_song_name.py
@@ -10,14 +10,15 @@ SN_TITLE_SIZE = 29
 SN_TITLE_HEIGHT = 296
 SN_TITLE_WIDTH = 48
 
-SN_SUBTITLE_SIZE = 26
+SN_SUBTITLE_SIZE = 22
 SN_SUBTITLE_HEIGHT = 296
 SN_SUBTITLE_WIDTH = 48
 
+BG_COLOR = 0xFFFFFF00
 TXT_COLOR = 0x000000FF
-FONT = "DFKTLB.ttc"
+FONT = "DFKTLB.TTC"
 
-ROTATE_CHARACTER = set(u"「」～（）ー-~")
+ROTATE_CHARACTER = set(u"「」～（）ー-~－")
 
 def convert(out_path, txt=u"Test", size=None, font=None, bgcolor=None, txt_color=None, hspacing=None, vspacing=None, align=None, font_size=None, rotate=0):
 	cmd = "convert.exe"
@@ -60,19 +61,19 @@ def vconvert(out_path, txt=u"Test", size=None, font=None, bgcolor=None, txt_colo
 	# join all tokens
 	os.system("convert.exe -gravity North -append %s test.png" % (" ".join(["token%d.png" % i for i in xrange(len(txt))])))
 	
+	# scale picture carefully
+	
 
 def gen_song_name_non_select(title, subtitle, out_folder):
-	vtitle = u"\\n".join(title)
-	convert(os.path.join(out_folder, "sn_non_select.png"), txt=vtitle,
+	vconvert2(os.path.join(out_folder, "sn_non_select.png"), txt=title,
 		size=(SN_TITLE_WIDTH, SN_TITLE_HEIGHT), font=FONT,
-		bgcolor=0x00000000, txt_color=TXT_COLOR, vspacing=-5, align="North",
+		bgcolor=BG_COLOR, txt_color=TXT_COLOR, vspacing=-5, align="North",
 		font_size=SN_TITLE_SIZE)
 
 def gen_song_name_select_short(title, subtitle, out_folder):
-	vtitle = u"\\n".join(title)
-	convert(os.path.join(out_folder, "sn_select_short.png"), txt=vtitle,
+	vconvert2(os.path.join(out_folder, "sn_select_short.png"), txt=title,
 		size=(80, SN_TITLE_HEIGHT), font=FONT,
-		bgcolor=0x00000000, txt_color=TXT_COLOR, vspacing=-5, align="North",
+		bgcolor=BG_COLOR, txt_color=TXT_COLOR, vspacing=-5, align="North",
 		font_size=SN_TITLE_SIZE)
 	
 def gen_song_name_select_full(title, subtitle, out_folder):
@@ -80,31 +81,32 @@ def gen_song_name_select_full(title, subtitle, out_folder):
 		vtitle = u"\\n".join(title)
 		convert(os.path.join(out_folder, "sn_select_full.png"), txt=vtitle,
 			size=(80, SN_TITLE_HEIGHT), font=FONT,
-			bgcolor=0x00000000, txt_color=TXT_COLOR, vspacing=-5, align="North",
+			bgcolor=BG_COLOR, txt_color=TXT_COLOR, vspacing=-5, align="North",
 			font_size=SN_TITLE_SIZE)
 	else:
 		vconvert2(os.path.join(out_folder, "sn_subtitle.png"), txt=subtitle,
 			size=(48, SN_SUBTITLE_HEIGHT), font=FONT,
-			bgcolor=0x00000000, txt_color=TXT_COLOR, vspacing=-4, align="South",
+			bgcolor=BG_COLOR, txt_color=TXT_COLOR, vspacing=-4, align="South",
 			font_size=SN_SUBTITLE_SIZE)
-		os.system("convert.exe -size 80x296 xc:#00000000 %s" % os.path.join(out_folder, "sn_select_full.png"))
-		os.system("composite.exe -gravity East %s %s %s" % (
+		os.system("convert.exe -size 80x296 xc:#%08x %s" % (BG_COLOR, os.path.join(out_folder, "sn_select_full.png")))
+		os.system("composite.exe -gravity Southwest %s %s %s" % (
+			os.path.join(out_folder, "sn_subtitle.png"),
+			os.path.join(out_folder, "sn_select_full.png"),
+			os.path.join(out_folder, "sn_select_full.png")
+			))		
+		os.system("composite.exe -gravity Northeast %s %s %s" % (
 			os.path.join(out_folder, "sn_non_select.png"),
 			os.path.join(out_folder, "sn_select_full.png"),
 			os.path.join(out_folder, "sn_select_full.png")
 			))		
-		os.system("composite.exe %s %s %s" % (
-			os.path.join(out_folder, "sn_subtitle.png"),
-			os.path.join(out_folder, "sn_select_full.png"),
-			os.path.join(out_folder, "sn_select_full.png")
-			))
+
 
 		#os.system("del %s" % os.path.join(out_folder, "sn_subtitle.png"))
 		
 def gen_song_name_enso(title, subtitle, out_folder):
 	convert(os.path.join(out_folder, "sn_game.png"), txt=title,
 		size=(SN_ENSO_WIDTH, SN_ENSO_HEIGHT), font=FONT,
-		bgcolor=0xFFFFFF00, txt_color=TXT_COLOR, hspacing=-3, align="East",
+		bgcolor=BG_COLOR, txt_color=TXT_COLOR, hspacing=-3, align="East",
 		font_size=SN_ENSO_SIZE)
 
 def gen_song_name_texture(title, subtitle, out_folder):
@@ -113,35 +115,76 @@ def gen_song_name_texture(title, subtitle, out_folder):
 	gen_song_name_select_short(title, subtitle, out_folder)
 	gen_song_name_select_full(title, subtitle, out_folder)
 	
-def gen_glyphs_from_text(text):
+def gen_glyphs_from_text(text, size):
+	ret = []
 	for token_idx, ch in enumerate(text):
 		if not ch.isspace():
-			gen_glyph.gen0(FONT, SN_TITLE_SIZE, ch, "token%d.png" % token_idx)
+			w, h = gen_glyph.gen0(FONT, size, ch, "token%d.png" % token_idx)
+			ret.append((w, h))
+		else:
+			ret.append((0, 0))
+	return ret
 	
 # vconvert version2
 # using freetype generated glyph(without extra space) to build a good vertical text
 def vconvert2(out_path, txt=u"Test", size=None, font=None, bgcolor=None, txt_color=None, hspacing=None, vspacing=None, align=None, font_size=None, rotate=0):
-	# make background texture
-	os.system("convert.exe -size %dx%d xc:#%08x %s" % (size[0], size[1], bgcolor, out_path))
-	
 	# gen character pic
-	gen_glyphs_from_text(txt)
+	glyph_sizes = gen_glyphs_from_text(txt, font_size)
 	
 	# handle special character
 	for token_idx, ch in enumerate(txt):
-		if ch.isspace():	# increase a small gap
-			os.system("convert.exe -size %dx%d xc:#%08x token%d.png" % (size[0], 3, bgcolor, token_idx))
+		if glyph_sizes[token_idx] == (0, 0):	# increase a small gap
+			os.system("convert.exe -size %dx%d xc:#%08x token%d.png" % (size[0], 4, bgcolor, token_idx))
+			glyph_sizes[token_idx] = (size[0], 4)
 			continue
 		if ch in ROTATE_CHARACTER:	# rotate if needed
 			os.system("convert.exe -rotate 90 token%d.png token%d.png" % (token_idx, token_idx))
+			glyph_sizes[token_idx] = (glyph_sizes[token_idx][1], glyph_sizes[token_idx][0])
 			continue
 					
 	# join all tokens
-	os.system("convert.exe -gravity South -append %s %s" % (" ".join(["token%d.png" % i for i in xrange(len(txt))]), out_path))
+	gravity = align or "North"
+	os.system("convert.exe -gravity %s -background #%08x -append %s %s" % (gravity, bgcolor, " ".join(["token%d.png" % i for i in xrange(len(txt))]), out_path))
+	os.system("convert.exe -gravity %s -background #%08x -append %s %s" % (gravity, bgcolor, " ".join(["token%d.png" % i for i in xrange(len(txt))]), "test.png"))
+	#os.system("del token*.png")
+	
+	# width and height
+	height = 0
+	width = 0
+	for w, h in glyph_sizes:
+		height += h
+		width = max(width, w)
 
+	print "size = (%d, %d)" % (width, height)
+	
+	# scaling carefully
+	scale = 1.0
+	scale = min(scale, 1.0 * size[0] / width)
+	scale = min(scale, 1.0 * size[1] / height)
+	if scale < 1.0:
+		os.system("convert.exe %s -resize %f%% %s" % (out_path, scale * 100, out_path))
+		width = int(width * scale)
+		height = int(height * scale)
+	print "scale = %f" % scale
+	print "scaled_size = (%d, %d)" % (width, height)
+	
+	# Adding border
+	border_x = abs((size[0] - width) // 2)
+	border_y = abs((size[1] - height) // 2)
+	if border_x != 0 or border_y != 0:
+		os.system("convert.exe %s -bordercolor #FFFFFF00 -border %dx0 %s" % (out_path, border_x, out_path))
+		os.system("convert.exe %s -gravity %s -background #FFFFFF00 -extent %dx%d %s" % (out_path, align, size[0], size[1], out_path))
+	
 if __name__ == '__main__':
-	gen_song_name_texture(u"カロン", u"ＴＶＣＭ「ＬＩＳＭＯ！」より", ".")
-	gen_song_name_texture(u"ガツガツ！！", u"「トリコ」より", ".")
+	#gen_song_name_texture(u"カロン", u"ＴＶＣＭ「ＬＩＳＭＯ！」より", ".")
+	#gen_song_name_texture(u"ガツガツ！！", u"「トリコ」より", ".")
+	#gen_song_name_texture(u"季曲", u"～Ｓｅａｓｏｎｓ　Ｏｆ　Ａｓｉａ～", ".")
+	#gen_song_name_texture(u"蓄勢", u"～Ｇｅａｒ Ｕｐ～", ".")
+	gen_song_name_texture(u"蓄勢（裏）", u"～Ｇｅａｒ Ｕｐ～", ".")
+	#gen_song_name_texture(u"蛻變", u"～Ｔｒａｎｓｆｏｒｍａｔｉｏｎ～", ".")
+	#gen_song_name_texture(u"Ｒｏｔｔｅｒ　Ｔａｒｍｉｎａｔｉｏｎ", u"　", ".")
+	#gen_song_name_texture(u"きがつけば　あなた", u"ＫＩＲＩＮ「午後の紅茶」ＣＭソング", ".")
+	#gen_song_name_texture(u"練習曲ＯＰ．１０－４", u"　", ".")    
 	#vconvert2("test.png", txt=u"ＴＶＣＭ「ＬＩＳＭＯ！」より",
 			#size=(80, SN_TITLE_HEIGHT), font=FONT,
 			#bgcolor=0x00000000, txt_color=TXT_COLOR, vspacing=-5, align="North",
